f6ee18d48a5ef38c4a689c474ecb4a58d2cdb477
Samuel Santos <samuelssan28@gmail.com>
2020-04-17 23:43:35 -0300

    Imagens de teste do algoritmo

--- Conjuto Dominante Minimo/main.py
+++ Conjuto Dominante Minimo/main.py
@@ -89,7 +89,7 @@ blue =  mpatches.Patch(color='blue', label='Não vão retransmitir')
 
 plt.legend(handles=[red,blue])
 
-plt.savefig("Grafo_Conjunto_Dominante.png", format="PNG") #salvando grafo
+plt.savefig("Grafo"+str(n) +"_Conjunto_Dominante.png", format="PNG") #salvando grafo
 plt.show()
 
  
